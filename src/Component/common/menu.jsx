8453970d081b21b9e620ab781449a10b267a7a36
pujianbo@qq.com <pujianbo@qq.com>
2018-03-19 22:44:24 +0800

    修改菜单

--- src/Component/common/menu.jsx
+++ src/Component/common/menu.jsx
@@ -6,51 +6,35 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 import '../../Scss/Common/menu.scss'
 
-let routeArr=['/form','/datalist']
+const defaultKey=location.hash.split('?')[0].toLocaleLowerCase().slice(2)
 
 export default class menu extends Component {
-  constructor(props) {
-    super(props);
-    routeArr[12]='/config'
-    routeArr[13]='/role'
-    routeArr[14]='/users'
-    routeArr[15]='/datalog'
-    console.log(hashHistory.getCurrentLocation().pathname);
-  }
 
   handleClick(e) {
-    console.log(e);
-    if(routeArr[e.key])
-      hashHistory.push(routeArr[e.key])
+      localStorage.syskeyPath=e.keyPath[1]
+      hashHistory.push(`/${e.key}`)
   }
   render() {
     return (<div className='menu clearfix'>
       <div className='menubox'>
-        <Menu onClick={this.handleClick.bind(this)} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline">
-          <SubMenu key="sub1" title={<span> <Icon type="team" /><span>医站管理</span></span>}>
-            <Menu.Item key="0" url='test'>表单提交</Menu.Item>
-            <Menu.Item key="1">数据列表</Menu.Item>
-            <Menu.Item key="2">医生列表</Menu.Item>
-            <Menu.Item key="3">医生列表</Menu.Item>
+        <Menu onClick={this.handleClick.bind(this)} defaultOpenKeys={[localStorage.syskeyPath]} defaultSelectedKeys={[defaultKey]} mode="inline">
+          <SubMenu key="sys" title={<span> <Icon type="setting" /><span>系统管理</span></span>}>
+            <Menu.Item key="config">网站配置</Menu.Item>
+            <Menu.Item key="role">角色管理</Menu.Item>
+            <Menu.Item key="users">用户管理</Menu.Item>
+            <Menu.Item key="datalog">系统日志</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" title={<span> <Icon type="edit" /><span>笔记管理</span></span>}>
-            <Menu.Item key="4">笔记分类</Menu.Item>
-            <Menu.Item key="5">笔记配置</Menu.Item>
-            <Menu.Item key="6">笔记配置</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" title={<span> <Icon type="line-chart" /><span>网站统计</span></span>}>
+          <SubMenu key="statistics" title={<span> <Icon type="line-chart" /><span>网站统计</span></span>}>
             <Menu.Item key="7">登录统计</Menu.Item>
             <Menu.Item key="8">购买统计</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub4" title={<span> <Icon type="database" /><span>数据分析</span></span>}>
+          <SubMenu key="data" title={<span> <Icon type="database" /><span>数据分析</span></span>}>
             <Menu.Item key="9">用户新增分析</Menu.Item>
             <Menu.Item key="10">模块使用分析</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub5" title={<span> <Icon type="setting" /><span>系统管理</span></span>}>
-            <Menu.Item key="11">网站配置</Menu.Item>
-            <Menu.Item key="12">角色管理</Menu.Item>
-            <Menu.Item key="13">用户管理</Menu.Item>
-            <Menu.Item key="14">系统日志</Menu.Item>
+          <SubMenu key="doctor" title={<span> <Icon type="team" /><span>模板使用</span></span>}>
+            <Menu.Item key="form">表单提交</Menu.Item>
+            <Menu.Item key="datalist">数据列表</Menu.Item>
           </SubMenu>
         </Menu>
       </div>
